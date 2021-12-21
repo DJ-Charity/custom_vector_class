28301776c0e77f9cb2ea838adffea8d14f4473f5
DJ-Charity <41906742+DJ-Charity@users.noreply.github.com>
2021-12-21 17:32:13 -0500

    Update customvector.cpp

--- customvector.cpp
+++ customvector.cpp
@@ -1,30 +1,10 @@
-./configure
+
 #include iostream
 
 using namespace std
 
-template <typename T>
-class vector<T> {
 
-  private:
-    vector<T> myVector;
-  
-  public:
-  
-    void add(T thing) {
-    
-      myVector.push_back(thing);
-      
-    }
-
-};
 
 int main() {
-
-  vector<T> newVector;
-  newVector.add(1);
-  
-  
-
-
+   cout << 105;
 }
